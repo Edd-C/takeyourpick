653fa5e522ba8266046892d5baafabd401b73f4d
EddC <edward.earl.clarke@gmail.com>
2020-04-11 22:49:55 -0700

    code

--- pages/voting.page.php
+++ pages/voting.page.php
@@ -6,16 +6,18 @@
 			<button type="button" class="btn btn-info btn-lg w-100" style="height:130px;"><a href="#TK" class="display-3 font-weight-bold" style="text-decoration: none; color:#fff;">TK</a></button>
 			<button type="button" class="btn btn-info btn-lg w-100" style="height:130px;"><a href="#SL" class="display-3 font-weight-bold" style="text-decoration: none; color:#fff;">SL</a></button>
 			<button type="button" class="btn btn-info btn-lg w-100" style="height:130px;"><a href="#KB" class="display-3 font-weight-bold" style="text-decoration: none; color:#fff;">KB</a></button>
+			<button type="button" class="btn btn-info btn-lg w-100" style="height:130px;">
+				<a href="/?page=scoreboard&userId=<?= isset($_GET['userId']) ? $_GET['userId'] : ''; ?>" class="font-weight-bold" style="text-decoration: none; color:#FFD700	;">
+					<svg class="bi bi-trophy" width="4em" height="4em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path d="M3 1h10c-.495 3.467-.5 10-5 10S3.495 4.467 3 1zm0 15a1 1 0 011-1h8a1 1 0 011 1H3zm2-1a1 1 0 011-1h4a1 1 0 011 1H5z"/>
+						<path fill-rule="evenodd" d="M12.5 3a2 2 0 100 4 2 2 0 000-4zm-3 2a3 3 0 116 0 3 3 0 01-6 0zm-6-2a2 2 0 100 4 2 2 0 000-4zm-3 2a3 3 0 116 0 3 3 0 01-6 0z" clip-rule="evenodd"/>
+						<path d="M7 10h2v4H7v-4z"/>
+						<path d="M10 11c0 .552-.895 1-2 1s-2-.448-2-1 .895-1 2-1 2 .448 2 1z"/>
+					</svg>
+				</a>
+			</button>
 		</div>
 	</nav>
-
-	<!-- Scoreboard Link -->
-    <div class="container-fluid">
-		<div class="row">
-			<a class="btn btn-info w-100 py-4" style="font-size:4em;" href="/?page=scoreboard&userId=<?= isset($_GET['userId']) ? $_GET['userId'] : ''; ?>" role="button">Scoreboard</a>
-		</div>
-    </div>
-
 <?php
 
 
