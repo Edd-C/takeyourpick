c166a54368a37407aca68faaae00e5d35dc64c5b
EddC <edward.earl.clarke@gmail.com>
2020-04-12 21:01:29 -0700

    code

---
+++ ajax/getAllVotes.php
@@ -0,0 +1,13 @@
+<?php
+include_once("../config/config.php");
+
+
+$stmt = $conn->prepare("SELECT * FROM pictures");
+$stmt->execute();
+
+$data = $stmt->get_result()->fetch_all( MYSQLI_ASSOC);
+
+$stmt->close();
+
+echo json_encode($data);
+?>
